7d909fbde9fe8a4cbed8b5e5582ef1ce3da74a00
CJ Ting <fatelovely1128@gmail.com>
2017-09-19 12:48:35 +0800

    Finish anagram && matrix

---
+++ anagram/anagram.exs
@@ -0,0 +1,21 @@
+defmodule Anagram do
+  @doc """
+  Returns all candidates that are anagrams of, but not equal to, 'base'.
+  """
+  @spec match(String.t, [String.t]) :: [String.t]
+  def match(base, candidates) do
+    candidates
+    |> Enum.filter(&is_anagram(base, &1))
+  end
+
+  # case insensitive
+  defp is_anagram(source, target) do
+    source_downcase = String.downcase(source)
+    target_downcase = String.downcase(target)
+    source_downcase != target_downcase and
+    get_sorted_charlist(source_downcase) == get_sorted_charlist(target_downcase)
+  end
+
+  defp get_sorted_charlist(str), 
+    do: str |> String.to_charlist |> Enum.sort
+end
